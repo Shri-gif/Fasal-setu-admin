5aaf2538265939987395b81c6bd1def8ddb0c629
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-08-28 02:07:53 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,35 @@
+/* =========================================================
+   FASAL SETU ADMIN
+   Supabase Connection
+========================================================= */
 
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+
+/* =========================================================
+   SUPABASE CONFIGURATION
+========================================================= */
+
+export const SUPABASE_URL =
+    "https://iyurbpfsvqzmdyaqinqi.supabase.co";
+
+export const SUPABASE_PUBLISHABLE_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzIiwicmVmIjoiaXl1cmJwZnN2cXp
+tZGF5YXFpbnFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczMjkyODUsImV4cCI6MjEwMjkwNTI4NX0.QiHk-cjLDETbK385RqW3R40A3ePpTn1B0XgN4FOJs2Q";
+
+
+/* =========================================================
+   SUPABASE CLIENT
+========================================================= */
+
+export const supabase = createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY,
+    {
+        auth: {
+            persistSession: true,
+            autoRefreshToken: true,
+            detectSessionInUrl: true
+        }
+    }
+);
