238bb23a17184259f9e30820cf9b3797cd1b6a87
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-08-28 02:09:03 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -14,8 +14,7 @@ export const SUPABASE_URL =
     "https://iyurbpfsvqzmdyaqinqi.supabase.co";
 
 export const SUPABASE_PUBLISHABLE_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzIiwicmVmIjoiaXl1cmJwZnN2cXp
-tZGF5YXFpbnFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczMjkyODUsImV4cCI6MjEwMjkwNTI4NX0.QiHk-cjLDETbK385RqW3R40A3ePpTn1B0XgN4FOJs2Q";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzIiwicmVmIjoiaXl1cmJwZnN2cXptZGF5YXFpbnFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczMjkyODUsImV4cCI6MjEwMjkwNTI4NX0.QiHk-cjLDETbK385RqW3R40A3ePpTn1B0XgN4FOJs2Q";
 
 
 /* =========================================================
